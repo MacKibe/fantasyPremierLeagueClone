d7193d33f98d9e90a172ec346dec668ca5dfd3ff
MacKibe <ndichujames2030@gmail.com>
2025-01-31 10:59:08 +0300

    updated

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import '../App.css'
+import Nav  from "./Nav"
+function App() {
+
+  return (
+  <div className="bg-orange-300">
+    <Nav />
+  </div>
+  )
+}
+
+export default App
---
+++ src/components/Nav.tsx
@@ -0,0 +1,11 @@
+const Nav = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-white p-3">
+        ScoreMasters.
+      </h1>
+    </div>
+  );
+};
+
+export default Nav;
